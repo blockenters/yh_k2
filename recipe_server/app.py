3c0189081fc36eb38fe62658e8ff653f59fc90c1
blockenters <blockenters@gmail.com>
2022-06-17 16:09:53 +0900

    add

--- recipe_server/app.py
+++ recipe_server/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_restful import Api
 
-from resources.recipe import RecipeListResource 
+from resources.recipe import RecipeListResource
+from resources.recipe_info import RecipeResource 
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ api = Api(app)
 
 # 경로와 리소스(API 코드)를 연결한다.
 api.add_resource(RecipeListResource, '/recipes')
+api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
 
 
 if __name__=="__main__" :
