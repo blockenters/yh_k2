f3076cd408dc183af4f531ed18c7e3793340eb7c
blockenters <blockenters@gmail.com>
2022-06-27 16:10:09 +0900

    add

--- posting-server/resources/posting.py
+++ posting-server/resources/posting.py
@@ -537,3 +537,67 @@ class PostingInfoResource(Resource) :
     def get(self, posting_id):
         return
 
+
+class PostingFollowResource(Resource) :
+    @jwt_required()
+    def get(self) :
+        # 1. 클라이언트로부터 데이터를 받아온다.
+        offset = request.args['offset']
+        limit = request.args['limit']
+        user_id = get_jwt_identity()
+
+        # 2. 디비로부터 데이터를 가져온다.
+        try :
+            connection = get_connection()
+
+            query = '''select p.*, u.email, u.name, 
+                    if(l.userId is null, 0, 1) as isLike, count(l2.postingId) as likeCnt
+                    from follow f
+                    join posting p 
+                    on f.followeeId = p.userId
+                    join user u 
+                    on p.userId = u.id
+                    left join likes l 
+                    on p.id = l.postingId and l.userId = %s 
+                    left join likes l2 
+                    on p.id = l2.postingId
+                    where f.followerId = %s
+                    group by p.id
+                    limit '''+offset+''' , '''+limit+''';'''
+            
+            record = (user_id, user_id)
+
+            # select 문은, dictionary = True 를 해준다.
+            cursor = connection.cursor(dictionary = True)
+
+            cursor.execute(query, record)
+
+            # select 문은, 아래 함수를 이용해서, 데이터를 가져온다.
+            result_list = cursor.fetchall()
+
+            print(result_list)
+
+            # 중요! 디비에서 가져온 timestamp 는 
+            # 파이썬의 datetime 으로 자동 변경된다.
+            # 문제는! 이데이터를 json 으로 바로 보낼수 없으므로,
+            # 문자열로 바꿔서 다시 저장해서 보낸다.
+            i = 0
+            for record in result_list :                
+                result_list[i]['createdAt'] = record['createdAt'].isoformat()
+                result_list[i]['updatedAt'] = record['updatedAt'].isoformat()
+                i = i + 1                
+
+            cursor.close()
+            connection.close()
+
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+
+            return {"error" : str(e), 'error_no' : 20}, 503
+
+        return {'result' : 'success' ,
+                'count' : len(result_list),
+                'items' : result_list}, 200
+
