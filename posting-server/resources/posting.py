17b8e43396a54033848afe9475904cc37ec86c6a
blockenters <blockenters@gmail.com>
2022-06-27 13:18:01 +0900

    Update posting.py

--- posting-server/resources/posting.py
+++ posting-server/resources/posting.py
@@ -1,3 +1,4 @@
+from ctypes import pointer
 from datetime import datetime
 from http import HTTPStatus
 from os import access
@@ -410,7 +411,67 @@ class PostingInfoResource(Resource) :
 
     @jwt_required()
     def delete(self, posting_id) :
-        return
+
+        # 1. 클라이언트로부터 데이터를 받아온다.
+        user_id = get_jwt_identity()
+
+        # 2. 디비에서 삭제!!
+        #    내가 작성한 포스팅만 삭제 가능해야 한다.
+        try :
+            # 데이터 삭제
+            # 1. DB에 연결
+            connection = get_connection()
+
+            # 2. 쿼리문 만들기
+            query = '''delete from posting
+                    where id = %s and userId = %s;'''
+            
+            record = ( posting_id, user_id )
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 2. 쿼리문 만들기
+            query = '''delete from likes
+                    where postingId = %s;'''
+            
+            record = ( posting_id, )
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 2. 쿼리문 만들기
+            query = '''delete from tag
+                    where postingId = %s;'''
+            
+            record = ( posting_id, )
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+            connection.commit()
+
+            # 6. 자원 해제
+            cursor.close()
+            connection.close()
+
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            return {'error' : str(e)}, 503
+
+        return {'result' : 'success'}
 
     @jwt_required()
     def get(self, posting_id):
