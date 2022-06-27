e2b1451c1feb5d0d8178e6491bf2b17b9562b003
blockenters <blockenters@gmail.com>
2022-06-27 18:02:28 +0900

    add

--- flask-openApi/app.py
+++ flask-openApi/app.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
 from config import Config
+from resources.naver import NaverPapagoResource
 
 app = Flask(__name__)
 
@@ -13,7 +14,7 @@ jwt = JWTManager(app)
 api = Api(app)
 
 # 경로와 클래스를 연결한다.
-
+api.add_resource(NaverPapagoResource, '/chinese')
 
 if __name__ == '__main__' :
     app.run()
