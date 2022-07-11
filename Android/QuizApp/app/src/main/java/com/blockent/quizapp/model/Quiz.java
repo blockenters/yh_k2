b4e3215928b99f80f51d346014917bfe7e77c5a8
blockenters <blockenters@gmail.com>
2022-07-11 11:51:08 +0900

    add

---
+++ Android/QuizApp/app/src/main/java/com/blockent/quizapp/model/Quiz.java
@@ -0,0 +1,12 @@
+package com.blockent.quizapp.model;
+
+public class Quiz {
+    public int question;
+    public boolean answer;
+
+
+    public Quiz(int question, boolean answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
