968913b569f201a5f6268657369718797d4c8149
blockenters <blockenters@gmail.com>
2022-07-12 17:08:45 +0900

    Update MainActivity.java

--- Android/Timer/app/src/main/java/com/blockent/timer/MainActivity.java
+++ Android/Timer/app/src/main/java/com/blockent/timer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.blockent.timer;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
@@ -52,6 +54,37 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("MyTimer", "타이머 끝났다!!");
 
                 isFinish = true;
+
+                // 알러트 다이얼로그 (팝업) 를 띄어서,
+                // 한번 더 할것인지, 앱을 종료할것인지 물어본다.
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("게임 종료!");
+                alert.setMessage("한번 더 하시겠습니까???");
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        isFinish = false;
+                        count = 0;
+                        txtCount.setText("탭 횟수 : " + count);
+                        timer.start();
+
+                    }
+                });
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // 현재 이 액티비티를 종료시키는 코드
+                        finish();
+                    }
+                });
+                // 이 알러트 다이얼로그의 버튼을
+                // 안누르면, 화면이 넘어가지 않도록하는 방법
+                alert.setCancelable(false);
+                // 알러트 다이얼로그를 화면에 표시
+                alert.show();
+
             }
         };
 
