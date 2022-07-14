59472b39154681500a3349ae0c687bc5b17a7a02
blockenters <blockenters@gmail.com>
2022-07-14 11:18:13 +0900

    add

---
+++ Android/ContactManager/app/src/main/java/com/blockent/contactmanager/model/Contact.java
@@ -0,0 +1,13 @@
+package com.blockent.contactmanager.model;
+
+public class Contact {
+    public int id;
+    public String name;
+    public String phone;
+
+    public Contact(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+}
