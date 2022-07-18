55bb364a932806175195e6731085b381694a9ae3
blockenters <blockenters@gmail.com>
2022-07-18 17:42:26 +0900

    add

--- Android/EmployerApp/app/src/main/java/com/blockent/employerapp/MainActivity.java
+++ Android/EmployerApp/app/src/main/java/com/blockent/employerapp/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 
 import com.android.volley.Request;
@@ -28,15 +30,15 @@ public class MainActivity extends AppCompatActivity {
     // 어댑터
     EmployeeAdapter adapter;
     // 어레이리스트
-    ArrayList<Employee> employeeList;
+    ArrayList<Employee> employeeList = new ArrayList<>();
 
     // 네트워크로 데이터 주고받을때 동글뱅이 표시용
     ProgressBar progressBar;
 
-    final String URL = "http://dummy.restapiexample.com/api/v1/employees";
+//    final String URL = "http://dummy.restapiexample.com/api/v1/employees";
 
     // 위의 URL 이 안되면
-//    final String URL = "https://block1-image-test.s3.ap-northeast-2.amazonaws.com/employees.json";
+    final String URL = "https://block1-image-test.s3.ap-northeast-2.amazonaws.com/employees.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
 
+                        Log.i("MyEmpApp", response.toString());
+
                         try {
                             JSONArray dataList = response.getJSONArray("data");
 
@@ -69,27 +73,38 @@ public class MainActivity extends AppCompatActivity {
                                 int age = data.getInt("employee_age");
                                 int salary = data.getInt("employee_salary");
 
-                                // 클래스의 객체로 만들어서
+                                // 클래스의 객체로 만들어서 (메모리에 저장)
                                 Employee employee = new Employee(id, name, age, salary);
 
                                 // 멤버변수인 리스트에 담아줘야, cpu가 일할수 있다.
-
+                                employeeList.add(employee);
                             }
 
                         } catch (JSONException e) {
                             e.printStackTrace();
+                            progressBar.setVisibility(View.INVISIBLE);
+                            return;
                         }
+
+                        // 어댑터 만들어서, 실제 화면에 데이터를 표시하도록 한다.
+                        adapter = new EmployeeAdapter(MainActivity.this, employeeList);
+                        recyclerView.setAdapter(adapter);
+
+                        progressBar.setVisibility(View.INVISIBLE);
 
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        Log.i("MyEmpApp", error.toString());
 
+                        progressBar.setVisibility(View.INVISIBLE);
                     }
                 }
         );
         // 네트워크 통신 실행하는 코드
+        progressBar.setVisibility(View.VISIBLE);
         queue.add(request);
     }
 }
