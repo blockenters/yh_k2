25441d602568124ccbfb2b7160aa5f9653357a6a
blockenters <blockenters@gmail.com>
2022-07-20 10:22:50 +0900

    add

---
+++ Android/Glide/app/src/main/java/com/blockent/glide/adapter/PhotoAdapter.java
@@ -0,0 +1,40 @@
+package com.blockent.glide.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PhotoAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtTitle;
+        TextView txtId;
+        TextView txtAlbumId;
+        ImageView imgThumb;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+}
---
+++ Android/Glide/app/src/main/java/com/blockent/glide/model/Photo.java
@@ -0,0 +1,11 @@
+package com.blockent.glide.model;
+
+public class Photo {
+
+    int albumId;
+    int id;
+    String title;
+    String url;
+    String thumbnailUrl;
+
+}
