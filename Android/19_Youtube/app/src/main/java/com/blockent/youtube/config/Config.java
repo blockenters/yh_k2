a88887868bc2c04c05e4e97647000c0592521995
blockenters <blockenters@gmail.com>
2022-07-20 16:08:33 +0900

    Delete Config.java

--- Android/19_Youtube/app/src/main/java/com/blockent/youtube/config/Config.java
+++
@@ -1,9 +0,0 @@
-package com.blockent.youtube.config;
-
-public class Config {
-
-    public static final String BASE_URL = "https://www.googleapis.com/youtube/v3/search";
-    public static final String GOOGLE_API_KEY = "AIzaSyAR8l-CGjmoLJfehQ1jr3fp3AQRnNtXenk";
-
-
-}
