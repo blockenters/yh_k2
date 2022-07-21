d9fcc078721be08f94706534d5e7413276a57eab
blockenters <blockenters@gmail.com>
2022-07-21 11:05:57 +0900

    add

--- Android/20_Papago/app/src/main/java/com/blockent/papago/MainActivity.java
+++ Android/20_Papago/app/src/main/java/com/blockent/papago/MainActivity.java
@@ -1,5 +1,6 @@
 package com.blockent.papago;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,12 +11,29 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     RadioGroup radioLang;
     EditText editText;
     Button button;
     TextView txtResult;
+
+    final String URL = "https://openapi.naver.com/v1/papago/n2mt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,10 +75,59 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 // 3. 네이버 API 호출해서
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
 
-                // 4. 결과를 텍스트뷰에 보여준다.
+                StringRequest request = new StringRequest(
+                        Request.Method.POST,
+                        URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // 4. 결과를 텍스트뷰에 보여준다.
+
+                                try {
+                                    JSONObject result = new JSONObject(response);
+                                    String translatedText = result.getJSONObject("message").getJSONObject("result").getString("translatedText");
+                                    txtResult.setText(translatedText);
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }
+                ){
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                        params.put("X-Naver-Client-Id", "9VCFeTBe_jFd3f9oNJX4");
+                        params.put("X-Naver-Client-Secret", "uMmxCY74ZL");
+                        return params;
+                    }
+
+                    @Nullable
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("source", source);
+                        params.put("target", target);
+                        params.put("text", text);
+                        return params;
+                    }
+                };
+
+                queue.add(request);
+
             }
         });
+
 
     }
 }
