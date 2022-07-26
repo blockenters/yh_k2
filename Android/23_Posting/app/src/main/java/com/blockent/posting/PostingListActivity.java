a31181b54607139113538e3b4987a102e64fd3b1
blockenters <blockenters@gmail.com>
2022-07-26 17:10:10 +0900

    add

--- Android/23_Posting/app/src/main/java/com/blockent/posting/PostingListActivity.java
+++ Android/23_Posting/app/src/main/java/com/blockent/posting/PostingListActivity.java
@@ -1,14 +1,79 @@
 package com.blockent.posting;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.blockent.posting.adapter.FollowPostingAdapter;
+import com.blockent.posting.model.Posting;
+
+import java.util.ArrayList;
 
 public class PostingListActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FollowPostingAdapter adapter;
+    ArrayList<Posting> postingList = new ArrayList<>();
+
+    ProgressBar progressBar;
+
+    // 페이징에 필요한 멤버변수
+    int count = 0;
+    int offset = 0;
+    int limit = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posting_list);
+
+        progressBar = findViewById(R.id.progressBar);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(PostingListActivity.this));
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int lastPosition = ((LinearLayoutManager)recyclerView.getLayoutManager()).findLastCompletelyVisibleItemPosition();
+                int totalCount = recyclerView.getAdapter().getItemCount();
+
+                if(  lastPosition+1  == totalCount  ){
+
+                    if(count == limit){
+                        // 네트워크 통해서, 데이터를 더 불러오면 된다.
+                        addNetworkData();
+                    }
+                }
+            }
+        });
     }
+
+    void getNetworkData(){
+
+        postingList.clear();
+        count = 0;
+        offset = 0;
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        
+
+    }
+
+    void addNetworkData(){
+
+    }
+
 }
+
+
+
+
+
+
---
+++ Android/23_Posting/app/src/main/java/com/blockent/posting/adapter/FollowPostingAdapter.java
@@ -0,0 +1,86 @@
+package com.blockent.posting.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.blockent.posting.R;
+import com.blockent.posting.config.Config;
+import com.blockent.posting.model.Posting;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class FollowPostingAdapter extends RecyclerView.Adapter<FollowPostingAdapter.ViewHolder> {
+
+    Context context;
+    List<Posting> postingList;
+
+    public FollowPostingAdapter(Context context, List<Posting> postingList) {
+        this.context = context;
+        this.postingList = postingList;
+    }
+
+    @NonNull
+    @Override
+    public FollowPostingAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.posting_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FollowPostingAdapter.ViewHolder holder, int position) {
+        Posting posting = postingList.get(position);
+        holder.txtContent.setText(posting.getContent());
+        holder.txtName.setText(posting.getName());
+        holder.txtCreatedAt.setText(posting.getCreatedAt());
+        holder.txtLikeCnt.setText(posting.getLikeCnt());
+
+        if(posting.getIsLike() == 1){
+            holder.imgLike.setImageResource(R.drawable.ic_thumb_up);
+        }else{
+            holder.imgLike.setImageResource(R.drawable.ic_thumb_up_off);
+        }
+
+        Glide.with(context).load(Config.IMAGE_URL+posting.getImgUrl() )
+                .placeholder(R.drawable.ic_default_photo)
+                .into(  holder.imgPhoto  );
+    }
+
+    @Override
+    public int getItemCount() {
+        return postingList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CardView cardView;
+        TextView txtContent;
+        TextView txtName;
+        TextView txtCreatedAt;
+        TextView txtLikeCnt;
+        ImageView imgPhoto;
+        ImageView imgLike;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            cardView = itemView.findViewById(R.id.cardView);
+            txtContent = itemView.findViewById(R.id.txtContent);
+            txtName = itemView.findViewById(R.id.txtName);
+            txtCreatedAt = itemView.findViewById(R.id.txtCreatedAt);
+            txtLikeCnt = itemView.findViewById(R.id.txtLikeCnt);
+            imgPhoto = itemView.findViewById(R.id.imgPhoto);
+            imgLike = itemView.findViewById(R.id.imgLike);
+
+        }
+    }
+}
